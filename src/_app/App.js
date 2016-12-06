3eaf9bb6ca6edb79be704416cc52f8b60312d711
theoperatore <theoperatore@gmail.com>
2016-12-06 01:15:19 -0500

    new version init

---
+++ src/_app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './app.less';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-'use strict';
-
 import React from 'react';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import FastClick from 'fastclick';
+import ReactDOM from 'react-dom';
+import App from './_app/App';
 
-import App from './App';
-import reducers from './state';
+import './index.css';
 
-const mount = document.querySelector('#mount');
-new FastClick(document.body);
-
-const characters = JSON.parse(localStorage.getItem('__dnd_companion_encounter_helper_players')) || [];
-
-const logger = createLogger();
-const store = createStore(reducers, { characters }, applyMiddleware(thunk, logger));
-
-store.subscribe(() => {
-  const toSave = JSON.stringify(store.getState().characters);
-  localStorage.setItem('__dnd_companion_encounter_helper_players', toSave);
-
-  render(<App dispatch={store.dispatch} state={store.getState()} />, mount);
-})
-
-render(<App dispatch={store.dispatch} state={store.getState()} />, mount);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
