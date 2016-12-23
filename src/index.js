438c6030d466e08581aacbca5d144a6dba3fa535
Al Petersen <al@spotify.com>
2016-12-23 09:18:59 -0600

    using local storage for tmp persistence

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import App from './_app/App';
 import createAppStore from './state';
 import './index.less';
 
+const prevState = localStorage.getItem('__dnd-encounter-helper-state__') || '{}';
+
 const root = document.getElementById('root');
-const store = createAppStore();
+const store = createAppStore(JSON.parse(prevState));
 
 FastClick.attach(root);
 
 function render() {
+  localStorage.setItem('__dnd-encounter-helper-state__', JSON.stringify(store.getState()));
   ReactDOM.render(
     <App
       state={store.getState()}
